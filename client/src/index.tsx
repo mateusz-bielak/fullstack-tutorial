129acba89524030eb7ec93bde4637f01837d7b9d
Mateusz Bielak <mateusz.bielak@tooploox.com>
2020-05-03 09:26:26 +0200

    5. Connect your API to a client 4. Run your graph in production

---
+++ client/src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
+
+import Pages from './pages';
+import injectStyles from './styles';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: 'http://localhost:4000/',
+});
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+    cache,
+    link,
+});
+
+injectStyles();
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <Pages />
+    </ApolloProvider>,
+    document.getElementById('root'),
+);
